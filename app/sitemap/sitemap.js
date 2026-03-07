e459709af6fed5b99bc0566cc636709b2d6a032b
Jake Dryka <jakedryka1@gmail.com>
2026-03-08 04:54:00 +0800

    add sitemap

---
+++ app/sitemap/sitemap.js
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://scamsmart.click",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://scamsmart.click/scamsmart",
+      lastModified: new Date(),
+    }
+  ];
+}
